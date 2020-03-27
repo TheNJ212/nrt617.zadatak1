aa9047755c35a946a79cbdcddc149e0fbc0a5200
TheNJ212 <46373506+TheNJ212@users.noreply.github.com>
2020-03-27 21:50:07 +0100

    Podizanje modula

---
+++ nrt617.zadatak1.js
@@ -0,0 +1,21 @@
+var math=require("mathjs");
+
+exports.RastojanjeIzmedjuDveTacke= (ax,ay,bx,by)=>{
+    return math.sqrt(math.square(bx-ax)-math.square(by-ay));
+}
+
+exports.Hipotenuza= (a,b)=>{
+    return math.sqrt(math.square(a)+math.square(b));
+}
+
+exports.PovrsinaKruga= (r)=>{
+    return math.square(r)*math.pi;
+}
+
+exports.ZapreminaValjka=(r,H)=>{
+    return this.PovrsinaKruga(r)*H;
+}
+
+exports.PovrsinaTroubla(a,b,angle){
+    return a*b*math.sin(angle)/2;
+}
